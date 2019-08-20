4476689419d5717b79d4c84962034677a07a69f3
rendiwibawa <rendiwibawa25@gmail.com>
2019-08-20 11:27:26 +0700

    first projek

---
+++ src/latihan_bil_terbesar/LATIHAN_BIL_TERBESAR.java
@@ -0,0 +1,19 @@
+package LATIHAN_BIL_TERBESAR;
+public class LATIHAN_BIL_TERBESAR{
+    public static void main(String[] args) {
+        int A,B;
+        
+        A=20;
+        B=10;
+        if (A>B);{
+      System.out.println("BILANGAN TERBESAR="+A);
+    
+    
+
+           
+
+    }
+    
+    
+    
+}
